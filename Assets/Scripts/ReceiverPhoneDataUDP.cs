d826588fc3613f40270ada22224c4ebadb3242bd
Pedro Omedas <pomedas.specs@gmail.com>
2021-03-02 23:02:15 +0100

    Initial commit

---
+++ Assets/Scripts/ReceiverPhoneDataUDP.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Globalization;
+
+public class ReceiverPhoneDataUDP : MonoBehaviour
+{
+	//Thread to receive by UDP from the Phone 
+	bool on = false;
+	private Thread lThread;
+
+	//UDP Receiver definitions
+	private UdpClient client;
+	private IPEndPoint remoteIpEndPoint;
+
+	//Receiver port
+	int port = 21800;
+
+	Quaternion receivedRotation; 
+
+    private void Start()
+    {
+		Listen();
+	}
+
+	public void Listen()
+	{
+		client = new UdpClient(port);
+		remoteIpEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+		lThread = new Thread(new ThreadStart(ListenUDPThread));
+		lThread.Name = "Receiver UDP listen thread";
+		lThread.Start();
+	}
+
+	private void ListenUDPThread()
+	{
+		var culture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+		//Debug.Log("separator: "+culture.NumberFormat.NumberDecimalSeparator);
+		on = true;
+
+		Debug.Log("Receiver: listening on port " + port);
+
+		while (on)
+		{
+			try
+			{
+				byte[] packet = client.Receive(ref remoteIpEndPoint);
+
+				// read message
+				if (packet != null && packet.Length > 0)
+				{
+					string message = ExtractString(packet, 0, packet.Length);
+					string[] messageSplit = message.Split(new Char[] { ',' });
+
+					receivedRotation = new Quaternion(float.Parse(messageSplit[0].ToString()),
+													float.Parse(messageSplit[1].ToString()),
+													float.Parse(messageSplit[2].ToString()),
+													float.Parse(messageSplit[3].ToString()));
+
+					//Debug.Log("Receiver: "+message);
+				}
+				if (packet != null && packet.Length == 0)
+				{
+					Debug.Log("Received packet is empty");
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.Log(e.ToString());
+			}
+		}
+	}
+
+	public void Close()
+	{
+		on = false;
+		lThread.Abort();
+		if (client != null) client.Close();
+	}
+
+	private string ExtractString(byte[] packet, int start, int length)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < packet.Length; i++) sb.Append((char)packet[i]);
+		return sb.ToString();
+	}
+
+    private void Update()
+    {
+		transform.rotation = receivedRotation;
+    }
+
+    private void OnDestroy()
+    {
+		Close();
+    }
+}
---
+++ Assets/Scripts/SendPhoneDataUDP.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendPhoneDataUDP : MonoBehaviour
+{
+    //UDP definitions
+    public string ipAddress;
+    int udpPort = 21800; 
+
+    private UdpClient sock; //UDP
+    private IPEndPoint iep; //UDP
+
+    //Variable to check if we have made the connection
+    bool connected = false;
+
+    //The input field game object
+    public InputField iPAddressInputField;
+
+    //Text to show messages
+    public Text msgText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Enables the use of the Gyroscope in the phone
+        Input.gyro.enabled = true;
+
+        //Initialize the defaul IP address
+        iPAddressInputField.text = "192.168.86.165";
+    }
+
+    public void Connect()
+    {
+        if (!connected)
+        {
+            try
+            {
+                //Get the ip address from the input field
+                ipAddress = iPAddressInputField.text;
+
+                //Initialize socket objects
+                sock = new UdpClient();
+                iep = new IPEndPoint(IPAddress.Parse(ipAddress), udpPort);
+
+                //
+                connected = true;
+
+                Debug.Log("Connected");
+            }
+            catch (Exception e) {
+                //Show the error in the GUI
+                msgText.text = e.Message.ToString();
+
+                Debug.Log("Exception in Connect: " + e.Message.ToString());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            Send("Test");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (connected)
+        {
+            Quaternion q = Input.gyro.attitude;
+            transform.rotation = new Quaternion(q.x, q.y, -q.z, -q.w);
+            string msg = (q.x) + "," + (q.y) + "," + (-q.z) + "," + (-q.w);
+            Send(msg);
+        }
+    }
+
+    public void Send(string message)
+    {
+        byte[] data = Encoding.ASCII.GetBytes(message);
+
+        try
+        {
+            // Note: read UDP ports with "nc".e.g. nc - lu 127.0.0.1 27015
+            sock.Send(data, data.Length, iep);
+            //Debug.Log("->" + message);
+        }
+        catch (Exception e)
+        {
+            //Show the error in the GUI
+            msgText.text = e.Message.ToString();
+            Debug.Log("Exception send: " + e.Message.ToString());
+        }
+
+    }
+}
